a3bbf33b920477b51bedfee7b6392c68bec7e07a
MykolaK <Konovalovmst@gmail.com>
2021-01-20 20:58:31 +0200

    task

--- tests/BasicsTest.php
+++ tests/BasicsTest.php
@@ -17,12 +17,15 @@ class BasicsTest extends TestCase
 
         // The function name, or {closure} for anonymous functions.
         // TODO __CLASS__
+        $this->assertEquals('BasicsTest', __CLASS__);
 
         // The class method name.
         // TODO __METHOD__
+        $this->assertEquals('BasicsTest::testMagicConstants', __METHOD__);
 
         // The name of the current namespace.
         // TODO __NAMESPACE__
+        $this->assertEquals('', __NAMESPACE__);
     }
 
     /**
@@ -38,18 +41,27 @@ class BasicsTest extends TestCase
         // TODO (bool) -1
         // TODO (bool) -10
         // TODO (bool) 0
+        $this->assertEquals(true, (bool) -1);
+        $this->assertEquals(true, (bool) -10);
+        $this->assertEquals(false, (bool) 0);
 
         // Strings
         // TODO (bool) ''
         // TODO (bool) 'false'
         // TODO (bool) 'not empty string'
+        $this->assertEquals(false, (bool) '');
+        $this->assertEquals(true, (bool) 'false');
+        $this->assertEquals(true, (bool) 'not emty string');
 
         // Arrays
         // TODO (bool) []
         // TODO (bool) [1, 2, 3]
+        $this->assertEquals(false, (bool) []);
+        $this->assertEquals(true, (bool) [1, 2, 3]);
 
         // Null
         // TODO (bool) null
+        $this->assertEquals(false, (bool) null);
     }
 
     /**
@@ -65,15 +77,19 @@ class BasicsTest extends TestCase
 
         // Multiplication
         // TODO to be implemented
+        $this->assertEquals(2, 2 * 1);
 
         // Division
         // TODO to be implemented
+        $this->assertEquals(3, 9 / 3);
 
         // Modulo
         // TODO to be implemented
+        $this->assertEquals(4, 39 % 7);
 
         // Exponentiation
         // TODO to be implemented
+        $this->assertEquals(32, 2 ** 5);
     }
 
     /**
@@ -100,9 +116,15 @@ class BasicsTest extends TestCase
         $this->assertIsInt($foo);
 
         // TODO $foo = $foo * 1.3;
+        $foo = $foo * 1.3;
+        $this->assertIsFloat($foo);
 
         // TODO $foo = 5 * (int) '10 Little Piggies';
+        $foo = 5 * (int) '10 Little Piggies';
+        $this->assertIsInt($foo);
 
         // TODO $foo = 5 * (int) '10 Small Pigs';
+        $foo = 5 * (int) '10 Small Pigs';
+        $this->assertIsInt($foo);
     }
 }
--- tests/StringsTest.php
+++ tests/StringsTest.php
@@ -18,12 +18,23 @@ class StringsTest extends TestCase
         $this->assertEquals('Hello $foo', 'Hello $foo');
 
         // TODO "Hello ${foo}"
+        $this->assertEquals('Hello world', "Hello ${foo}");
 
         // TODO "Hello " . $foo
+        $this->assertEquals('Hello world', "Hello " . $foo);
 
         // TODO Heredoc
-
+        $str = <<<EOT
+        $foo
+        EOT;
+        $this->assertEquals('Hello world', "Hello $str");
+        
         // TODO Nowdoc
+        $str = <<<'EOT'
+        world
+        EOT;
+        $this->assertEquals('Hello world', "Hello $str");
+
     }
 
     /**
@@ -37,9 +48,13 @@ class StringsTest extends TestCase
 
         // ltrim — Strip whitespace (or other characters) from the beginning of a string
         // TODO to be implemented
+        $this->assertEquals('to be implemented', ltrim('       to be implemented'));
+        $this->assertEquals('to be implemented', ltrim('......to be implemented', '.'));
 
         // rtrim — Strip whitespace (or other characters) from the end of a string
         // TODO to be implemented
+        $this->assertEquals('to be implemented', rtrim('to be implemented         '));
+        $this->assertEquals('to be implemented', rtrim('to be implemented......', '.'));
 
         // strtoupper — Make a string uppercase
         $this->assertEquals('HELLO', strtoupper('hello'));
@@ -49,41 +64,57 @@ class StringsTest extends TestCase
 
         // ucfirst — Make a string's first character uppercase
         // TODO to be implemented
+        $this->assertEquals('To be implemented', ucfirst('to be implemented'));
 
         // lcfirst — Make a string's first character lowercase
         // TODO to be implemented
+        $this->assertEquals('to be implemented', lcfirst('To be implemented'));
 
         // strip_tags — Strip HTML and PHP tags from a string
         // TODO to be implemented
+        $this->assertEquals('to be implemented', strip_tags('<p>to be</p><!-- Комментарий --> <a href="#fragment">implemented</a>'));
 
         // htmlspecialchars — Convert special characters to HTML entities
         // TODO to be implemented
+        $this->assertEquals('&amp; to be implemented &lt;', htmlspecialchars('& to be implemented <'));
 
         // addslashes — Quote string with slashes
         // TODO to be implemented
+        $this->assertEquals('to \"be \\\implemented', addslashes('to "be \implemented'));
 
         // strcmp — Binary safe string comparison
         // TODO to be implemented
+        $this->assertEquals(0, strcmp('to be implemented', 'to be implemented') !== 0);
 
         // strncasecmp — Binary safe case-insensitive string comparison of the first n characters
         // TODO to be implemented
+        $this->assertEquals('to be implemented', ucfirst('TO bE Implemented'));
 
         // str_replace — Replace all occurrences of the search string with the replacement string
         // TODO to be implemented
+        $this->assertEquals('to be implemented', str_replace("implement", "implemented", "to be implement"));
 
         // strpos — Find the position of the first occurrence of a substring in a string
         // TODO to be implemented
+        $this->assertEquals(1, strpos("to be implemented", "o"));
 
         // strstr — Find the first occurrence of a string
         // TODO to be implemented
-
+        $this->assertEquals('/manual/ru/function.strstr.php', substr("https://www.php.net/manual/ru/function.strstr.php", 'manual'));
         // strrchr — Find the last occurrence of a character in a string
         // TODO to be implemented
+        $this->assertEquals('/function.strrchr.php', substr("https://www.php.net/manual/ru/function.strrchr.php", '/'));
 
         // substr — Return part of a string
         // TODO to be implemented
+        $this->assertEquals('Hello', substr("Hello World", 0, 5)); 
 
         // sprintf — Return a formatted string
         // TODO to be implemented
+        $num = 3;
+        $location = 'test';
+        $format = 'to be implemented %d %s';
+        $this->assertEquals('to be implemented 3 test', sprintf($format, $num, $location));
+
     }
 }
